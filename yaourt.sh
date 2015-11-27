8e174d2f6d8c5225bb56c9cc6c66a773f4f83a1a
Brenton Horne <brentonhorne77@gmail.com>
2015-11-28 08:43:05 +1000

    Update yaourt.sh

--- yaourt.sh
+++ yaourt.sh
@@ -1,5 +1,7 @@
+sudo pacman -Sy git
+git clone https://aur.archlinux.org/package-query.git
 cd package-query
 makepkg -s && sudo pacman -U *.pkg.tar.xz --noconfirm
+git clone https://aur.archlinux.org/yaourt.git
 cd ../yaourt
 makepkg -s && sudo pacman -U *.pkg.tar.xz --noconfirm
-
