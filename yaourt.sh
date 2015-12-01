51b530944ff6cf791759e62a0b88e48e0482234f
Brenton Horne <brentonhorne77@gmail.com>
2015-12-01 21:55:32 +1000

    Turning into a function

--- yaourt.sh
+++ yaourt.sh
@@ -1,7 +1,11 @@
-sudo pacman -Sy git
-git clone https://aur.archlinux.org/package-query.git
-cd package-query
-makepkg -s && sudo pacman -U *.pkg.tar.xz --noconfirm
-git clone https://aur.archlinux.org/yaourt.git
-cd ../yaourt
-makepkg -s && sudo pacman -U *.pkg.tar.xz --noconfirm
+function yaset {
+  sudo pacman -Sy git
+  git clone https://aur.archlinux.org/package-query.git
+  cd package-query
+  makepkg -s && sudo pacman -U *.pkg.tar.xz --noconfirm
+  cd ..
+  git clone https://aur.archlinux.org/yaourt.git
+  cd yaourt
+  makepkg -s && sudo pacman -U *.pkg.tar.xz --noconfirm
+  cd ..
+}
