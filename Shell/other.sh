a5b8e508001ca7edf3de7d9179bc24ffefce7644
fusion809 <brentonhorne77@gmail.com>
2015-12-26 20:57:52 +1000

    Updating scripts

--- Shell/other.sh
+++ Shell/other.sh
@@ -1,3 +1,23 @@
 function jekex {
   bundle exec jekyll serve
 }
+
+function ps_mem {
+  sudo python $HOME/GitHub/ps_mem/ps_mem.py -p $@
+}
+
+function ps_gax {
+  ps ax | grep $@
+}
+
+function ps_find {
+  pidof $@ | tr '\ ' ','
+}
+
+function ps_ap {
+  ps_mem $(ps_find $@)
+}
+
+function ps_apl {
+  ps_mem ${$(pgrep $@ | tr '\n' ,)%,*}
+}
