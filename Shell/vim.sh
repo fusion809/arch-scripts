f804770b06ec488d4c96c67d32d73d0c2477f50e
fusion809 <brentonhorne77@gmail.com>
2016-01-28 22:14:59 +1000

    Updating scripts

--- Shell/vim.sh
+++ Shell/vim.sh
@@ -1,11 +1,23 @@
-function nvim {
-	nano ~/.vimrc
-}
-
 function avim {
 	atom ~/.vimrc
 }
 
+function nvim {
+        nano ~/.vimrc
+}
+
 function vvim {
 	vim ~/.vimrc
+}
+
+function vbash {
+	vim ~/.bashrc
+}
+
+function vsh {
+	vim -c ":call SH()"
+}
+
+function vzsh {
+	vim ~/.zshrc
 }
