7599e6dd910a9a14f72b40dadacbca096aeb6ee2
fusion809 <brentonhorne77@gmail.com>
2016-01-29 17:18:25 +1000

    Updating scripts

--- Shell/aur.sh
+++ Shell/aur.sh
@@ -1,3 +1,7 @@
 function cppk {
 	cp -a ~/GitHub/PKGBUILDs/${PWD##*/}/{PKGBUILD,*.install,*.patch,*.h} ~/AUR/${PWD##*/}
 }
+
+function cpau {
+	cp -a ~/AUR/${PWD##*/}/{PKGBUILD,*.install} ~/GitHub/PKGBUILDs/${PWD##*/}/
+}
