7951a9028634e2f20e447ba5ebad3801a4bead86
fusion809 <brentonhorne77@gmail.com>
2016-02-08 02:00:55 +1000

    Updating cpm script

--- Shell/git.sh
+++ Shell/git.sh
@@ -124,12 +124,15 @@ function pushss {
   }
 
 	function cpm {
-		sudo cp -a /etc/xdg/menus/{cinnamon,lxde,xfce}-applications.menu /home/fusion809/GitHub/LXDE-menu/etc/xdg/menus/
-		sudo cp -a /usr/share/desktop-directories/* /home/fusion809/GitHub/LXDE-menu/usr/share/desktop-directories/
+		sudo cp -a /etc/xdg/menus/{cinnamon,lxde,xfce}-applications.menu /home/fusion809/GitHub/xdg-menus/etc/xdg/menus/
+		sudo cp -a /usr/share/desktop-directories/* /home/fusion809/GitHub/xdg-menus/usr/share/desktop-directories/
+        cp -a ~/.local/share/applications/* ~/GitHub/xdg-menus/.local/share/applications/
+        cp -a ~/.local/share/desktop-directories/* ~/GitHub/xdg-menus/.local/share/desktop-directories
+        cp -a ~/.config/menus/applications-kmenuedit.menu ~/GitHub/xdg-menus/.config/menus/
 	}
 
 	function cdm {
-		cd /home/fusion809/GitHub/LXDE-menu/
+		cd /home/fusion809/GitHub/xdg-menus/
 	}
 
 	function mup {
