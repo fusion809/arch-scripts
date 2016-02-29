b0020617269933a40480d6bafa75d07cf663106d
fusion809 <brentonhorne77@gmail.com>
2016-02-29 13:47:17 +1000

    Updating scripts

--- .zshrc
+++ .zshrc
@@ -50,7 +50,7 @@ ZSH_THEME="hcompact"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git zsh-syntax-highlighting safe-paste)
+plugins=(git zsh-syntax-highlighting safe-paste zsh-completions)
 
 # User configuration
 
--- Shell/pms/pkgbuilds.sh
+++ Shell/pms/pkgbuilds.sh
@@ -4,17 +4,38 @@ function atomup {
   pushd ~/Programs/atom
   git checkout stable
 	git pull origin stable
-	verl=$(git describe --tags | sed 's/^v//;s/-/./g')
+	verls=$(git describe --tags | sed 's/^v//;s/-/./g')
+  git checkout beta
+  git pull origin beta
+  verlb=$(git describe --tags | sed 's/^v//;s/-/./g')
   popd
   pushd ~/GitHub/PKGBUILDs/atom-editor
-  verc=$(sed -n 's/pkgver=//p' PKGBUILD)
-  if [[ $verc == $verl ]]; then
+  vercs=$(sed -n 's/pkgver=//p' PKGBUILD)
+  if [[ $vercs == $verls ]]; then
 			echo "Atom is up-to-date"
   else
-    sed -i -e "s/pkgver=$verc/pkgver=$verl/g" PKGBUILD
-    rm v$verc.tar.gz
+    sed -i -e "s/pkgver=$vercs/pkgver=$verls/g" PKGBUILD
+    rm v$vercs.tar.gz
     updpkgsums
-    push "Bumping to $verl"
+    push "[atom-editor] Bumping to $verls"
+  fi
+  popd
+  pushd ~/GitHub/PKGBUILDs/atom-editor-beta
+  vercbb=$(sed -n 's/_pkgver=//p' PKGBUILD)
+  vercbr=$(sed -n 's/_pkgrel=//p' PKGBUILD)
+  vercb=$vercbb.$vercbr
+  vercd=$vercbb-$vercbr
+  if [[ $vercb == $verlb ]]; then
+      echo "Atom Beta is up-to-date"
+  else
+    verlbb=${verlb%.*}
+    verlbr=${verlb##*beta}
+    verlbd=$verlbb-beta$verlbr
+    sed -i -e "s/_pkgver=$vercbb/_pkgver=$verlbb/g" \
+           -e "s/_pkgrel=$vercbr/_pkgrel=$verlbr/g" PKGBUILD
+    rm v$vercd.tar.gz
+    updpkgsums
+    push "[atom-editor-beta] Bumping to $verlbd"
   fi
   popd
 }
