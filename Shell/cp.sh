71801459b75de77458f720d5857f87217e43d033
fusion809 <brentonhorne77@gmail.com>
2016-03-14 20:05:20 +1000

    Updating scripts

--- Shell/cp.sh
+++ Shell/cp.sh
@@ -1,7 +1,11 @@
 function cpobs {
-	if ! [[ -n $2 ]]; then
-		cp -r /var/abs/community/$1 ~/OBS/home:fusion809
-	else
-		cp -r /var/abs/$2/$1 ~/OBS/home:fusion809
-	fi
+	cdobs
+	for i in "$@"
+	do
+		pbget $i
+		cp -a package-query/_service $i
+                osc add $i
+                cd $i
+                osc ci -m "Initial commit"
+	done
 }
