95971af9109970257327b99476bafa8ac816d1c4
fusion809 <brentonhorne77@gmail.com>
2016-03-17 19:05:45 +1000

    Updating scripts

--- Shell/cp.sh
+++ Shell/cp.sh
@@ -10,3 +10,16 @@ function cpobs {
 		cd ..
 	done
 }
+
+function cpobsm {
+	cd ~/OBS/home:fusion809:arch_extra_multilib
+	for i in "$@"
+	do
+		pbget $i
+		cp -a ../home:fusion809:arch_extra/package-query/_service $i
+		osc add $i
+		cd $i
+		osc ci -m "Initial commit"
+		cd ..
+	done
+}
