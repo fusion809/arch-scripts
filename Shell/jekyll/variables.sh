7b0bfb3a26771ed4a6e43e7900051b2185cd01ab
fusion809 <brentonhorne77@gmail.com>
2016-03-22 20:30:59 +1000

    Updating scripts

--- Shell/jekyll/variables.sh
+++ Shell/jekyll/variables.sh
@@ -1,2 +1,1 @@
 export FGI=$GHUB/fusion809.github.io
-export GEMPATH=/root/.gem/ruby/2.3.0/bin:$HOME/.gem/ruby/2.3.0/bin
--- Shell/variables.sh
+++ Shell/variables.sh
@@ -2,6 +2,7 @@ export GHUB=$HOME/GitHub
 export ARS=$GHUB/arch-scripts
 export PKG=$GHUB/PKGBUILDs
 export CHROOT=$HOME/chroot
+export GEMPATH=/root/.gem/ruby/2.3.0/bin:$HOME/.gem/ruby/2.3.0/bin
 export GOPATH=$HOME
 export GOROOT=/usr/lib/go
 export PATH=$PATH:/usr/lib/jvm/java-7-openjdk/bin/:$GOPATH/bin:$GOROOT/bin:$GEMPATH
