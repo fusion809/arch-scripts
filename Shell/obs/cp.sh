1652f018b916680d4d2a668e2f2789fb863c1c2c
fusion809 <brentonhorne77@gmail.com>
2016-03-24 18:02:24 +1000

    Updating scripts

---
+++ Shell/obs/cp.sh
@@ -0,0 +1,36 @@
+function cpobs {
+	cd ~/OBS/home:fusion809:arch_extra
+	for i in "$@"
+	do
+		pbget --aur $i
+		cp -a package-query/_service $i
+    osc add $i
+    cd $i
+  	osc ci -m "Initial commit"
+		cd ..
+	done
+}
+
+function cpobsm {
+	cd ~/OBS/home:fusion809:arch_extra_multilib
+	for i in "$@"
+	do
+		pbget $i
+		cp -a ../home:fusion809:arch_extra/package-query/_service $i
+		osc add $i
+		cd $i
+		osc ci -m "Initial commit"
+		cd ..
+	done
+}
+
+function cpserv {
+	cp -a $OBS/package-query/_service .
+}
+
+function obsa {
+	PD=${PWD##*/}
+	pushd ..
+	osc add $PD
+	popd
+}
---
+++ Shell/obs/osc.sh
@@ -0,0 +1,44 @@
+function vimup {
+  unset verc
+  unset verl
+  unset VIMDIR
+  ORPWD=$PWD
+  pushd ~/GitHub/vim-src # change into my local Vim git repo copy
+  git checkout master
+  git fetch -p
+  git pull origin master # Update local repo
+  # verc is the current patch version of Vim in vim.spec
+  # verl is the latest patch version of Vim.
+  VIMDIR=/home/fusion809/OBS/home:fusion809/vim
+  verc=$(sed -n 's/%define patchlevel\s\s*//p' $VIMDIR/vim.spec)
+  verl=$(git describe --abbrev=0 --tags | sed 's/v7.4.//g')
+  popd
+  if [[ $verc == $verl ]]; then
+    echo "Vim is up-to-date"
+  else
+    pushd $VIMDIR
+      sed -i -e "s/%define patchlevel  $verc/%define patchlevel  $verl/g" vim.spec
+      osc ci -m "Updating to version 7.4.$verl"
+    popd
+    pushd $PKG/gvim
+      sed -i -e "s/pkgver=7.4.*/pkgver=7.4.$verl/g" PKGBUILD
+      rm *.*z
+      updpkgsums
+      makepkg -si --noconfirm --needed
+      push "[gvim] Bumping to 7.4.$verl"
+    popd
+    pushd $OBS/gvim
+      sed -i -e "s/pkgver=7.4.*/pkgver=7.4.$verl/g" PKGBUILD
+      rm *.*z
+      updpkgsums
+      osc ci -m "Bumping to 7.4.$verl"
+    popd
+  fi
+}
+
+function cpc {
+	for i in "$@"
+	do
+		osc copypac home:fusion809 $i home:fusion809:arch_extra $i
+	done
+}
