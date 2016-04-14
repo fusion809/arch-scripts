611efef59386a537d1ffeba87d3f591c297f4c36
fusion809 <brentonhorne77@gmail.com>
2016-04-15 00:08:39 +1000

    Updating scripts

--- Shell/variables/PATH.sh
+++ Shell/variables/PATH.sh
@@ -5,5 +5,5 @@ export GOROOT=/usr/lib/go
 export JVM_HOME=/usr/lib/jvm
 export JAVA_HOME=$JVM_HOME/default/bin:$JVM_HOME/java-7-openjdk/bin/
 export PERL_PATH=$HOME/perl5/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
-export PATH=$HOME/perl5/bin:/usr/local/bin:/usr/lib/hardening-wrapper/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:$GOROOT/bin:$GEMPATH:$PERL_PATH:$JAVA_HOME
+export PATH=$HOME/perl5/bin:/usr/local/bin:/usr/lib/hardening-wrapper/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:$GOROOT/bin:$GEMPATH:$PERL_PATH:$JAVA_HOME:$HOME/.cargo/bin/
 #:$HOME/bin
