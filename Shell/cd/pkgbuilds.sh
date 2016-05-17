0511a78278de68918666441915ea7712deb114db
fusion809 <brentonhorne77@gmail.com>
2016-05-17 15:04:55 +1000

    Updating scripts

--- Shell/cd/pkgbuilds.sh
+++ Shell/cd/pkgbuilds.sh
@@ -3,13 +3,13 @@ function cdpk {
 }
 
 function cdatp {
-	cd $PKG/atom-editor
+	cd $PKG/atom-editor/stable
 }
 
 function cdatpb {
-	cd $PKG/atom-editor-beta
+	cd $PKG/atom-editor/beta
 }
 
 function cdgg {
-  cd $PKG/gvim-git
+  cd $PKG/gvim/git
 }
