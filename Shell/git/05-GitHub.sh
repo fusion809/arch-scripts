c7bd4e086483b2fb40d084b0982c595e96db4c4d
fusion809 <brentonhorne77@gmail.com>
2016-06-09 19:19:00 +1000

    Updating scripts

--- Shell/git/05-GitHub.sh
+++ Shell/git/05-GitHub.sh
@@ -1,23 +1,112 @@
+containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
 function gitc {
+	export _packaging=('atom-editor'
+						         'atom-editor-arch'
+        						 'atom-editor-beta'
+        						 'atom-editor-beta-arch'
+        						 'atom-editor-fusion'
+        						 'atom-editor-git'
+        						 'atom-editor-nuclide'
+        						 'atom-editor-platformio'
+        						 'atom-editor-sync'
+        						 'atom-editor-web'
+        						 'blockify'
+        						 'brackets'
+        						 'codebox'
+        						 'gvim'
+        						 'gvim-git'
+        						 'gvim-gtk3'
+        						 'komodo-edit'
+        						 'lighttable'
+        						 'linux-ck'
+        						 'messengerfordesktop'
+        						 'PKGBUILDs'
+        						 'ps_mem'
+        						 'rpmbuild'
+        						 'sabayon-tools'
+        						 'shadow-icon-theme'
+        						 'shadow-icon-theme-git'
+        						 'sublime-text'
+        						 'sublime-text-dev'
+        						 'textadept'
+        						 'visual-studio-code'
+        						 'visual-studio-code-git'
+        						 'visual-studio-code-oss')
+
+  export _atom=('fusion-ui'
+                'language-archlinux'
+                'language-gfm2'
+                'language-ini-desktop'
+                'language-liquid'
+                'language-patch2'
+                'language-shellscript'
+                'terminal-fusion')
+
 	for i in "$@"
 	do
+    # mkdir $GHUBM
 		if ! [[ -d $GHUBM ]]; then
 			mkdir -p $GHUBM
 		fi
+
+    if ! [[ -d $GHUBM/atom ]]; then
+      mkdir -p $GHUBM/atom
+    fi
+
+    if ! [[ -d $GHUBM/packaging ]]; then
+      mkdir -p $GHUBM/packaging
+    fi
+
 		if [[ $i == 'atom' ]]; then
+      # Atom config repo
 			if ! [[ -d $HOME/.atom ]]; then
 				mkdir $HOME/.atom
+        git clone https://github.com/fusion809/atom-editor-arch /tmp/atom-editor-arch
+        pushd /tmp/atom-editor-arch
+        makepkg -sif --noconfirm
+        popd
 			fi
-			cd $HOME/.atom
+			pushd $HOME/.atom
 			gitsw
 			git pull origin master
-			cd -
+			popd -
+
+		elif `containsElement "$i" "${_atom[@]}"`; then
+      # Atom package/themes repos
+      if ! [[ -d $GHUBM/atom/$i ]]; then
+        git clone https://github.com/fusion809/$i.git $GHUBM/atom/$i
+        pushd $GHUBM/atom/$i
+        gitsw
+        popd
+      else
+        pushd $GHUBM/atom/$i
+        git pull origin master
+        popd
+      fi
+
+		elif `containsElement "$i" "${_packaging[@]}"`; then
+      # Packaging repos
+      if ! [[ -d $GHUBM/packaging/$i ]]; then
+        git clone https://github.com/fusion809/$i.git $GHUBM/packaging/$i
+        pushd $GHUBM/packaging/$i
+        gitsw
+        popd
+      else
+        pushd $GHUBM/atom/$i
+        git pull origin master
+        popd
+      fi
 		elif ! [[ -d $GHUBM/$i ]]; then
 			git clone https://github.com/fusion809/$i.git $GHUBM/$i
+      pushd $GHUBM/$i
+      gitsw
+      popd
 		fi
-		pushd $GHUBM/$i
-		gitsw
-		popd
 	done
 }
 
