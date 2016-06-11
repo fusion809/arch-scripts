bc348a06a36836818cc29ff583cd0a7d1ef1b154
fusion809 <brentonhorne77@gmail.com>
2016-06-11 18:40:24 +1000

    Updating scripts

--- Shell/edit/atom/GitHub/mine/atom/packages.sh
+++ Shell/edit/atom/GitHub/mine/atom/packages.sh
@@ -1,3 +1,12 @@
 function edapk {
 	atom $ATO/about-arch $ATO/language-archlinux $ATO/language-ini-desktop $ATO/language-patch2 $ATO/language-shellscript
 }
+
+function edapn {
+  atom $ATO/$1
+}
+
+function edapl {
+  LIST=$(for i in "$@"; do $ATO/$i \; done)
+  atom $LIST
+}
