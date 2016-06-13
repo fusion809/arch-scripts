ff113fc7f05eee14352bc54596755d66e7789d46
fusion809 <brentonhorne77@gmail.com>
2016-06-13 22:44:52 +1000

    Updating scripts

--- Shell/edit/atom/GitHub/mine/atom/packages.sh
+++ Shell/edit/atom/GitHub/mine/atom/packages.sh
@@ -15,6 +15,10 @@ function edapk {
 	edapl about browser-plus-fix language-archlinux language-gfm2 language-ini-desktop language-patch2 language-shellscript language-solus terminal-fusion
 }
 
+function edargm {
+  edapl language-gfm2
+}
+
 function edarpk {
   edapn language-archlinux
 }
