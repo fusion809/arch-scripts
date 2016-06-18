6d5d44ea68b78216222d603a907f77a83089d3f1
fusion809 <brentonhorne77@gmail.com>
2016-06-19 08:50:19 +1000

    Updating scripts

--- Shell/git/02-push.sh
+++ Shell/git/02-push.sh
@@ -1,4 +1,7 @@
 function push {
+  if `printf "$PWD" | grep 'AUR' > /dev/null 2>&1`; then
+    mksrcinfo
+  fi
   git add --all                                        # Add all files to git
   git commit -m "$1"                                   # Commit with message = argument 1
   git push origin $(git rev-parse --abbrev-ref HEAD)   # Push to the current branch
