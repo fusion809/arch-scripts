cdf6fbda7ca08ecd1d2f69a35e3b3e17021f89f1
fusion809 <brentonhorne77@gmail.com>
2016-06-21 10:06:57 +1000

    Placing vimup in pms/pkgbuilds

--- Shell/obs/osc.sh
+++ Shell/obs/osc.sh
@@ -46,39 +46,6 @@ function vimups {
   fi
 }
 
-function vimup {
-  unset verc
-  unset verl
-  unset VIMDIR
-  ORPWD=$PWD
-  pushd $GHUBO/vim-src # change into my local Vim git repo copy
-  git checkout master
-  git fetch -p
-  git pull origin master # Update local repo
-  # verc is the current patch version of Vim in vim.spec
-  # verl is the latest patch version of Vim.
-
-  verc=$(sed -n 's/pkgver=7.4.//p' $PKG/../gvim/PKGBUILD)
-  verl=$(git describe --abbrev=0 --tags | sed 's/v7.4.//g')
-  popd
-  if [[ $verc == $verl ]]; then
-    echo "Vim is up-to-date"
-  else
-    pushd $PK/gvim
-      sed -i -e "s/pkgver=7.4.*/pkgver=7.4.$verl/g" PKGBUILD
-      rm *.*z ../gvim-gtk3/*.*z
-      updpkgsums
-      cp v7.4.$verl.tar.gz ../gvim-gtk3
-    popd
-    pushd $PK/gvim-gtk3
-      sed -i -e "s/pkgver=7.4.*/pkgver=7.4.$verl/g" PKGBUILD
-      updpkgsums
-      makepkg -si --noconfirm --needed
-      push "[gvim/gvim-gtk3] Bumping to 7.4.$verl"
-    popd
-  fi
-}
-
 function cpc {
 	for i in "$@"
 	do
