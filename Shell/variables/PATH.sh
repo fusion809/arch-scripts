327b232469a547f3763573c6e82e07e727bad50b
fusion809 <brentonhorne77@gmail.com>
2016-07-09 12:04:41 +1000

    Updating scripts

--- Shell/variables/PATH.sh
+++ Shell/variables/PATH.sh
@@ -1,6 +1,6 @@
 unset PATH
 export GEMPATH=/root/.gem/ruby/2.3.0/bin:$HOME/.gem/ruby/2.3.0/bin
-export GOPATH=$HOME
+export GOPATH=$HOME/go
 export GOROOT=$GOPATH/bin
 export JVM_HOME=/usr/lib/jvm
 export JAVA_HOME=$JVM_HOME/default/bin:$JVM_HOME/java-7-openjdk/bin/
