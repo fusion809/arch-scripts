4fc9c7ec790b879255415dcbe80bec35b04aa408
fusion809 <brentonhorne77@gmail.com>
2016-07-12 17:37:14 +1000

    Updating scripts

--- Shell/pms/repo.sh
+++ Shell/pms/repo.sh
@@ -14,21 +14,3 @@ function repup {
     repo-add pkgbuild-current.db.tar.gz *.pkg.tar.xz
   fi
 }
-
-function grup {
-  if [[ -n "$1" ]]; then
-    gothub upload -t current -n "$@" -f "$@"
-  else
-    printf "You must provide an input!\n If you would like to upload all .pkg.tar.xz files in $PWD, run:\n grup *.pkg.tar.xz.\n"
-  fi
-}
-
-function gruppc {
-  if ! [[ $PWD == "$PKG/pkgbuild-current" ]]; then
-    cd $PKG/pkgbuild-current
-  fi
-  gothub upload -t current -n "pkgbuild-current.db.tar.gz" -f "pkgbuild-current.db.tar.gz" -R
-  gothub upload -t current -n "pkgbuild-current.db" -f "pkgbuild-current.db" -R
-  gothub upload -t current -n "pkgbuild-current.files.tar.gz" -f "pkgbuild-current.files.tar.gz" -R
-  gothub upload -t current -n "pkgbuild-current.files" -f "pkgbuild-current.files" -R
-}
