84a3347a3213a12579ec95c25b29462dd84961d2
fusion809 <brentonhorne77@gmail.com>
2016-07-12 14:19:14 +1000

    Updating scripts

--- Shell/pms/repo.sh
+++ Shell/pms/repo.sh
@@ -17,7 +17,7 @@ function repup {
 
 function grup {
   if [[ -n "$1" ]]; then
-    github-release upload -t current -n "$@" -f "$@"
+    gothub upload -t current -n "$@" -f "$@"
   else
     printf "You must provide an input!\n If you would like to upload all .pkg.tar.xz files in $PWD, run:\n grup *.pkg.tar.xz.\n"
   fi
@@ -27,8 +27,8 @@ function gruppc {
   if ! [[ $PWD == "$PKG/pkgbuild-current" ]]; then
     cd $PKG/pkgbuild-current
   fi
-  github-release upload -t current -n "pkgbuild-current.db.tar.gz" -f "pkgbuild-current.db.tar.gz"
-  github-release upload -t current -n "pkgbuild-current.db" -f "pkgbuild-current.db"
-  github-release upload -t current -n "pkgbuild-current.files.tar.gz" -f "pkgbuild-current.files.tar.gz"
-  github-release upload -t current -n "pkgbuild-current.files" -f "pkgbuild-current.files"
+  gothub upload -t current -n "pkgbuild-current.db.tar.gz" -f "pkgbuild-current.db.tar.gz" -R
+  gothub upload -t current -n "pkgbuild-current.db" -f "pkgbuild-current.db" -R
+  gothub upload -t current -n "pkgbuild-current.files.tar.gz" -f "pkgbuild-current.files.tar.gz" -R
+  gothub upload -t current -n "pkgbuild-current.files" -f "pkgbuild-current.files" -R
 }
