c26ea790244dca8b2395ce53a15abc0f6d230a1e
fusion809 <brentonhorne77@gmail.com>
2016-10-01 13:37:17 +1000

    Updating

---
+++ root/Shell/pacaur.sh
@@ -0,0 +1,7 @@
+function pacaup {
+	su - fusion809 -c "pacaur -Syu --noconfirm --force --needed --noedit"
+}
+
+function update {
+	su - fusion809 -c "pacaur -Syu --noconfirm --force --needed --noedit && apm-bleeding update --no-confirm"
+}
