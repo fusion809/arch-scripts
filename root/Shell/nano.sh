3672ba2342934eebf6ae03010a80a9087f1d311c
fusion809 <brentonhorne77@gmail.com>
2016-10-03 21:54:39 +1000

    Updating

---
+++ root/Shell/nano.sh
@@ -0,0 +1,11 @@
+function npc {
+	nano /etc/pacman.conf
+}
+
+function nps {
+	nano /etc/sddm.conf
+}
+
+function ns {
+	nano /etc/sudoers
+}
--- root/Shell/pacaur.sh
+++ root/Shell/pacaur.sh
@@ -5,3 +5,7 @@ function pacaup {
 function update {
 	su - fusion809 -c "pacaur -Syu --noconfirm --force --needed --noedit && apm-bleeding update --no-confirm && vimup"
 }
+
+function pacain {
+	su - fusion809 -c "pacaur -S $@ --noconfirm --force --needed --noedit"
+}
