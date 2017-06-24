2536e2c5d003590f11506c96029e5d963e48e319
fusion809 <fusion809@users.noreply.github.com>
2017-06-25 01:17:33 +1000

    Fixing cpi3

--- Shell/git/09-config.sh
+++ Shell/git/09-config.sh
@@ -1,8 +1,6 @@
 function cpi3 {
     rm -rf $CFG/i3-configs/.i3
-    cp $HOME/.Xresources $CFG/i3-configs
     cp $HOME/.xsession $CFG/i3-configs
-    cp $HOME/.i3status.conf $CFG/i3-configs
     cp -a $HOME/.i3 $CFG/i3-configs
     sudo cp /usr/local/bin/optirun-0ad $CFG/i3-configs/usr/local/bin
     sudo cp /usr/local/bin/rs-launcher $CFG/i3-configs/usr/local/bin
