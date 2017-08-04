d8f5c06387d5e44467e9e29b3ec4e60e949c7bde
fusion809 <fusion809@users.noreply.github.com>
2017-08-04 22:39:49 +1000

    Fixing spaces

--- Shell/cd/github/mine/pkgbuilds.sh
+++ Shell/cd/github/mine/pkgbuilds.sh
@@ -1,20 +1,16 @@
 # $GHUBM/packaging/PKGBUILDs repo
 function cdpk {
-	cd $PKG/$1
+    cd $PKG/$1
 }
 
 function cdate {
-	cdpk atom-editor
+    cdpk atom-editor
 }
 
 function cdatpb {
-	cdpk atom-editor-beta
+    cdpk atom-editor-beta
 }
 
 function cdgg {
-  cdpk gvim-git
-}
-
-function cdpc {
-	cdpk pkgbuild-current
+    cdpk gvim-git
 }
