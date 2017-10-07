50a5eda6dfe68c11fd3e2da39a394d0cb72975ad
fusion809 <fusion809@users.noreply.github.com>
2017-10-07 20:20:25 +1000

    Updating update func

--- Shell/pms/17-update.sh
+++ Shell/pms/17-update.sh
@@ -1,4 +1,5 @@
 function update {
+    # pacaur
     if ! `hash pacaur 2>/dev/null`; then
          cda cower
          makepkg -sifC --noconfirm
@@ -8,12 +9,26 @@ function update {
 
     pacaup
 
+    # Nix
     if hash nix-env 2>/dev/null; then
          sudo nix-channel --update && sudo nix-env --upgrade
     fi
 
+    # APM
     if hash apm 2>/dev/null; then
          apmup
     fi
-}
 
+    # Vim
+    cdobsh vim
+    pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
+    baseversion=$(echo $pkgver | sed 's/\.[0-9]*$//g')
+    patchversion=$(echo $pkgver | sed "s/$baseversion//g" | sed 's/\.//g')
+
+    vim_baseversion=$(cat vim.spec | grep "%define.*baseversion" | sed 's/%define.*baseversion\s*//g' | head -n 1)
+    vim_patchversion=$(cat vim.spec | grep "%define.*patchlevel" | sed 's/%define.*patchlevel\s*//g' | head -n 1)
+
+    if ! [ $pkgver == ${vim_baseversion}.${vim_patchversion} ]; then
+         vimup
+    fi
+}
