b0451aefde26e8dd980c9797daaf2b143234105b
fusion809 <fusion809@users.noreply.github.com>
2017-10-07 20:30:10 +1000

    Updating update func

--- Shell/pms/17-update.sh
+++ Shell/pms/17-update.sh
@@ -19,16 +19,21 @@ function update {
          apmup
     fi
 
-    # Vim
-    cdobsh vim
-    pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
-    baseversion=$(echo $pkgver | sed 's/\.[0-9]*$//g')
-    patchversion=$(echo $pkgver | sed "s/$baseversion//g" | sed 's/\.//g')
+    # Updating GitHub repos
+    for i in $GHUBO/*
+    do
+         pushd $i
+         git pull origin --all
+         popd
+    done
 
-    vim_baseversion=$(cat vim.spec | grep "%define.*baseversion" | sed 's/%define.*baseversion\s*//g' | head -n 1)
-    vim_patchversion=$(cat vim.spec | grep "%define.*patchlevel" | sed 's/%define.*patchlevel\s*//g' | head -n 1)
+    # Update firefox-nightly
+    pushd $HOME/.cache/pacaur/firefox-nightly
+         fntime=$(ls | grep "bz2$" | cut -d '-' -f 1)
+         actime=$(date +%Y%m%d)
 
-    if ! [ $pkgver == ${vim_baseversion}.${vim_patchversion} ]; then
-         vimup
-    fi
+         if ! [[ $fntime == $actime ]]; then
+              makepkg -sifC --noconfirm
+         fi
+    popd
 }
