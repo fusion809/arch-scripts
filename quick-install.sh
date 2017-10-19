608c44d647070ceb961ad7ebce85b333446a67cf
fusion809 <fusion809@users.noreply.github.com>
2017-10-19 21:00:38 +1000

    Comments

--- quick-install.sh
+++ quick-install.sh
@@ -30,8 +30,8 @@ else
   cd -
 fi
 
-if ! [[ -d $HOME/GitHub/mine/scripts/zsh-theme ]]; then
 # Get my self-made zsh-themes
+if ! [[ -d $HOME/GitHub/mine/scripts/zsh-theme ]]; then
   git clone https://github.com/fusion809/zsh-theme $HOME/GitHub/mine/scripts/zsh-theme
   cp -a $HOME/GitHub/mine/scripts/zsh-theme/*.zsh-theme $HOME/.oh-my-zsh/themes/
 else
@@ -41,15 +41,17 @@ else
   cp -a $HOME/GitHub/mine/scripts/zsh-theme/*.zsh-theme $HOME/.oh-my-zsh/themes/
 fi
 
+# Get zsh-syntax-highlighting
 if ! [[ -d $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting ]]; then
-  # Get zsh-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 fi
 
+# Get zsh-history-substring-search
 if ! [[ -d $HOME/.oh-my-zsh/plugins/zsh-history-substring-search ]]; then
   git clone https://github.com/zsh-users/zsh-history-substring-search $HOME/.oh-my-zsh/plugins/zsh-history-substring-search
 fi
 
+# Get zsh-completion-generator
 if ! [[ -d $HOME/.oh-my-zsh/custom/zsh-completion-generator ]]; then
   git clone https://github.com/RobSis/zsh-completion-generator $HOME/.oh-my-zsh/custom/zsh-completion-generator
 fi
