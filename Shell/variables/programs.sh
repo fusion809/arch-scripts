475d0458f42ef72011375efacbbfcaafed134db2
fusion809 <fusion809@users.noreply.github.com>
2017-11-03 13:40:41 +1000

    Adding XDG_DATA_DIR var

--- Shell/variables/programs.sh
+++ Shell/variables/programs.sh
@@ -2,3 +2,4 @@ export EDITOR=vim
 # GUI Editor - used by the `e` command
 export GUIEDITOR=atom
 export GUIX_LOCPATH=${HOME}/.guix-profile/lib/locale
+export XDG_DATA_DIRS=$XDG_DATA_DIR:$HOME/.guix-profile/share
