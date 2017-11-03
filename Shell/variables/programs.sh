6685d916c7e074280d569b59f4f74f18588d3d65
fusion809 <fusion809@users.noreply.github.com>
2017-11-03 13:50:48 +1000

    Adding GUILE_LOAD* vars

--- Shell/variables/programs.sh
+++ Shell/variables/programs.sh
@@ -3,3 +3,6 @@ export EDITOR=vim
 export GUIEDITOR=atom
 export GUIX_LOCPATH=${HOME}/.guix-profile/lib/locale
 export XDG_DATA_DIRS=$XDG_DATA_DIRS:$HOME/.guix-profile/share
+export GUILE_LOAD_PATH=$HOME/.guix-profile/share/guile/site/2.2:$GUILE_LOAD_PATH
+export GUILE_LOAD_COMPILED_PATH=$HOME/.guix-profile/lib/guile/2.2/site-ccache:$GUILE_LOAD_COMPILED_PATH
+
