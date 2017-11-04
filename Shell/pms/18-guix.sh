388740261bcc16835f4edc450c8bba108436d41e
fusion809 <fusion809@users.noreply.github.com>
2017-11-05 03:10:03 +1000

    Adding check to see if Guix is up to date to guixup

--- Shell/pms/18-guix.sh
+++ Shell/pms/18-guix.sh
@@ -1,5 +1,14 @@
 function guixup {
-    guix pull && guix package -u
+    BUILT=$(guix --version)
+    BUILT=$(echo $BUILT | head -n 1 | cut -d ' ' -f 4)
+    CURRENT=$(wget -cqO- https://git.savannah.gnu.org/git/guix.git/info/refs | grep "master" | sed 's|refs/heads/master||g')
+    CURRENT=$(echo $CURRENT | head -n 1)
+
+    if ! [[ $CURRENT == $BUILT ]]; then
+         guix pull && guix package -u
+    else
+         "Guix is up-to-date!"
+    fi
 }
 
 function guixupc {
