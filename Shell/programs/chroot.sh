5163738274539af13f2ff9ee50facd0b07a25b13
fusion809 <fusion809@users.noreply.github.com>
2017-11-12 00:20:39 +1000

    Adding froot funct

--- Shell/programs/chroot.sh
+++ Shell/programs/chroot.sh
@@ -12,3 +12,17 @@ function groot {
 
     sudo chroot /gentoo /usr/local/bin/su-fusion809
 }
+
+function froot {
+    if ! [[ -f /fedora/proc/config.gz ]]; then
+        sudo mount -t proc /proc /fedora/proc
+        sudo mount --rbind /dev /fedora/dev
+        sudo mount --make-rslave /fedora/dev
+        sudo mount --rbind /sys /fedora/sys
+        sudo mount --make-rslave /fedora/sys
+        sudo rm /fedora/etc/resolv.conf
+        sudo cp /etc/resolv.conf /fedora/etc
+    fi
+
+    sudo chroot /fedora /bin/bash
+}
