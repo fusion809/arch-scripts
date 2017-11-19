2a83cbdadbbb750836e3aedd950302490982cc4a
fusion809 <fusion809@users.noreply.github.com>
2017-11-19 11:00:55 +1000

    Redefining i3up and swup as I'll be using symlinks from now on

--- Shell/git/09-config.sh
+++ Shell/git/09-config.sh
@@ -1,18 +1,7 @@
-function cpi3 {
-    rm -rf $CFG/i3-configs/.i3
-    cp $HOME/.xsession $CFG/i3-configs
-    cp -a $HOME/.i3 $CFG/i3-configs
-}
-
 function i3up {
-    cdci3 && git checkout archlinux && cpi3 && push "$1" && cd -
-}
-
-function cpsw {
-    cp /usr/local/bin/swimport $CFG/sway-config/usr/local/bin
-    cp $HOME/.config/sway/config $CFG/sway-config
+    cdci3 && git checkout archlinux && push "$1" && cd -
 }
 
 function swup {
-    cdcsw && git checkout archlinux && cpsw && push "$1" && cd -
+    cdcsw && git checkout archlinux && push "$1" && cd -
 }
