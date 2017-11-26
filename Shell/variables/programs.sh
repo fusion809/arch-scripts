f6929965bfd5eeaf3a393e58191d9d123f073559
fusion809 <fusion809@users.noreply.github.com>
2017-11-26 13:14:11 +1000

    Rm guix so changing environment variables

--- Shell/variables/programs.sh
+++ Shell/variables/programs.sh
@@ -1,8 +1,4 @@
 export EDITOR=vim
 # GUI Editor - used by the `e` command
-export GUIEDITOR=atom
-export GUIX_LOCPATH=${HOME}/.guix-profile/lib/locale
-export XDG_DATA_DIRS=$XDG_DATA_DIRS:$HOME/.guix-profile/share:$HOME/.nix-profile/share
-export GUILE_LOAD_PATH=$HOME/.guix-profile/share/guile/site/2.2:$GUILE_LOAD_PATH
-export GUILE_LOAD_COMPILED_PATH=$HOME/.guix-profile/lib/guile/2.2/site-ccache:$GUILE_LOAD_COMPILED_PATH
-
+export GUIEDITOR=gvim
+export XDG_DATA_DIRS=$XDG_DATA_DIRS:$HOME/.nix-profile/share
--- root/Shell/variables.sh
+++ root/Shell/variables.sh
@@ -1,3 +1,1 @@
 export CHROOT=/home/fusion809/chroot
-export PATH=$PATH:~root/.guix-profile/bin
-export GUIX_LOCPATH=${HOME}/.guix-profile/lib/locale
