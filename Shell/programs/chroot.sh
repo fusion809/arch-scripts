193d0ebec384b7c49d1b4752afc25e2a947be802
fusion809 <fusion809@users.noreply.github.com>
2017-11-26 21:25:27 +1000

    Fixing Gentoo-related chroot functs

--- Shell/programs/chroot.sh
+++ Shell/programs/chroot.sh
@@ -1,16 +1,15 @@
 function groot {
-    if ! [[ -f /gentoo-runit/proc/config.gz ]]; then
-        sudo mount -t proc /proc /gentoo-runit/proc
-        sudo mount --rbind /dev /gentoo-runit/dev
-        sudo mount --make-rslave /gentoo-runit/dev
-        sudo mount --rbind /sys /gentoo-runit/sys
-        sudo mount --make-rslave /gentoo-runit/sys
-        sudo rm /gentoo-runit/etc/resolv.conf
-        sudo cp /etc/resolv.conf /gentoo-runit/etc
-        sudo chroot /gentoo-runit /usr/local/bin/md
+    if ! [[ -f /gentoo/proc/config.gz ]]; then
+        sudo mount -t proc /proc /gentoo/proc
+        sudo mount --rbind /dev /gentoo/dev
+        sudo mount --make-rslave /gentoo/dev
+        sudo mount --rbind /sys /gentoo/sys
+        sudo mount --make-rslave /gentoo/sys
+        sudo rm /gentoo/etc/resolv.conf
+        sudo cp /etc/resolv.conf /gentoo/etc
     fi
 
-    sudo chroot /gentoo-runit /usr/local/bin/su-fusion809
+    sudo chroot /gentoo /usr/local/bin/su-fusion809
 }
 
 function vroot {
@@ -36,7 +35,6 @@ function grroot {
         sudo mount --make-rslave /gentoo-runit/sys
         sudo rm /gentoo-runit/etc/resolv.conf
         sudo cp /etc/resolv.conf /gentoo-runit/etc
-        sudo chroot /gentoo-runit /usr/local/bin/md
     fi
 
     sudo chroot /gentoo-runit /usr/local/bin/su-fusion809
