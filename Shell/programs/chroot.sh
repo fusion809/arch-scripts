227c9237e8e730077cf1a149565a9111abf898ea
fusion809 <fusion809@users.noreply.github.com>
2017-12-08 08:23:03 +1000

    grroot->oroot

--- Shell/programs/chroot.sh
+++ Shell/programs/chroot.sh
@@ -26,17 +26,17 @@ function vroot {
     sudo chroot /void /usr/local/bin/su-fusion809
 }
 
-# Irrelevant now
-#function grroot {
-#    if ! [[ -f /gentoo-runit/proc/config.gz ]]; then
-#        sudo mount -t proc /proc /gentoo-runit/proc
-#        sudo mount --rbind /dev /gentoo-runit/dev
-#        sudo mount --make-rslave /gentoo-runit/dev
-#        sudo mount --rbind /sys /gentoo-runit/sys
-#        sudo mount --make-rslave /gentoo-runit/sys
-#        sudo rm /gentoo-runit/etc/resolv.conf
-#        sudo cp /etc/resolv.conf /gentoo-runit/etc
-#    fi
-#
-#    sudo chroot /gentoo-runit /usr/local/bin/su-fusion809
-#}
+# Whatever my "other" distro install is
+function oroot {
+    if ! [[ -f /other/proc/config.gz ]]; then
+        sudo mount -t proc /proc /other/proc
+        sudo mount --rbind /dev /other/dev
+        sudo mount --make-rslave /other/dev
+        sudo mount --rbind /sys /other/sys
+        sudo mount --make-rslave /other/sys
+        sudo rm /other/etc/resolv.conf
+        sudo cp /etc/resolv.conf /other/etc
+    fi
+
+    sudo chroot /other /usr/local/bin/su-fusion809
+}
