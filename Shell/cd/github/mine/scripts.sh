a8f12be33d0b5ec4cf27bf0a1a7154b5bf72d798
fusion809 <brentonhorne77@gmail.com>
2017-12-23 20:58:09 +1000

    Adding and improving on script-related cd funcs

--- Shell/cd/github/mine/scripts.sh
+++ Shell/cd/github/mine/scripts.sh
@@ -1,41 +1,51 @@
-# Arch setup repo
-function cdast {
-	cd $SCR/arch-setup
-}
-
-# My arch-scripts repo
-function cdarch {
-	cd $SCR/arch-scripts/$1
-}
-
-# My gentoo-scripts repo
-function cdgs {
-	cd $SCR/gentoo-scripts/$1
-}
-
-# - GNU_Octave
-function cdoct {
-	cd $SCR/GNU_Octave
-}
-
-# My JavaScripts
-function cdjs {
-	cd $SCR/JScripts
-}
-
-# My Python scripts repo
-function cdpy {
-	cd $SCR/python-scripts
-}
-
 # Change into $SCR
 function cdsc {
 	cd $SCR/$1
 }
 
+# Arch setup repo
+function cdast {
+    cdsc "arch-setup/$1"
+}
+
+# My arch-scripts repo
+function cdarch {
+    cdsc "arch-scripts/$1"
+}
+
+# fedora-scripts repo
+function cdfs {
+    cdsc "fedora-scripts/$1"
+}
+
+# My gentoo-scripts repo
+function cdgs {
+    cdsc "gentoo-scripts/$1"
+}
+
+# - GNU_Octave
+function cdoct {
+    cdsc "GNU_Octave/$1"
+}
+
+# My JavaScripts
+function cdjs {
+    cdsc "JScripts/$1"
+}
+
+# openSUSE scripts
+function cdos {
+    cdsc "opensuse-scripts/$1"
+}
+
+# My Python scripts repo
+function cdpy {
+    cdsc "python-scripts/$1"
+}
+
 # Lua scripts
 function cdlu {
-	cdsc "lua-scripts/$1"
+    cdsc "lua-scripts/$1"
 }
 
 # Void sripts
