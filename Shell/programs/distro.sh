645d0be71fec0df1c95fa17a1743e8010399d3f6
fusion809 <brentonhorne77@gmail.com>
2018-01-03 07:01:39 +1000

    Adding distr-list func

--- Shell/programs/distro.sh
+++ Shell/programs/distro.sh
@@ -1,3 +1,7 @@
 function distro-count {
     ls -ld /dev/disk/by-label/* | grep -v "sdb1" | grep -v "boot" | wc -l
 }
+
+function distro-list {
+    ls -ld /dev/disk/by-label/* | grep -v "sdb1" | grep -v "boot" | cut -d '/' -f 5 | cut -d ' ' -f 1 | sed 's/\\x20/ /g'
+}
