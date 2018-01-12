0b0f3548ee53cc1c959c214c6ed1fcfa49d05385
fusion809 <brentonhorne77@gmail.com>
2018-01-12 11:43:53 +1000

    Fixing chroot funcs PS1, also adding a few distro (ls -ld /dev/disk/by-label/*) funcs

--- Shell/programs/distro.sh
+++ Shell/programs/distro.sh
@@ -1,7 +1,26 @@
+function distro-func {
+    ls -ld /dev/disk/by-label/* | grep -v "sdb1" | grep -v "boot"
+}
+
 function distro-count {
-    ls -ld /dev/disk/by-label/* | grep -v "sdb1" | grep -v "boot" | wc -l
+    distro-func | wc -l
 }
 
 function distro-list {
-    ls -ld /dev/disk/by-label/* | grep -v "sdb1" | grep -v "boot" | cut -d '/' -f 5 | cut -d ' ' -f 1 | sed 's/\\x20/ /g'
+    distro-func | cut -d '/' -f 5 | cut -d ' ' -f 1 | sed 's/\\x20/ /g'
+}
+
+function distro-partition {
+    distros=$(distro-list)
+    count=$(distro-count)
+    partition=$(distro-func | cut -d ' ' -f 11 | cut -d '/' -f 3 | sed 's|sda|/dev/sda|g')
+    k=0
+    for i in $(seq 1 $count)
+    do 
+         part=$(echo $partition | head -n $i | tail -n 1)
+         distro=$(echo $distros | head -n $i | tail -n 1)
+         printf "$distro -> "
+         printf '\e[1;36m%-6s\e[m' "$part"
+         printf "\n"
+    done
 }
