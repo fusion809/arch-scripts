cd49dee47c45b7c578fc7521d0a7b7945a713d5a
Brenton Horne <brentonhorne77@gmail.com>
2018-12-19 01:55:17 +1000

    Transferring some aliases from yaourt to yay Making yaourt aliases only exist, if and only if, yaourt is installed, otherwise they belong to yay, my preferred AUR helper, as it is still supported.

--- Shell/pms/16-yaourt.sh
+++ Shell/pms/16-yaourt.sh
@@ -1,61 +1,63 @@
-function yaar {
-    yaourt -Qdtq | yaourt -Rs -
-}
+if command -v yaourt &> /dev/null ; then
+	function yaar {
+	    yaourt -Qdtq | yaourt -Rs -
+	}
 
-function yaset {
-    # Install git
-    sudo pacman -Sy git --noconfirm
-    # git package-query
-    git clone https://aur.archlinux.org/package-query.git
-    # change into package-query
-    cd package-query
-    # makepkg install
-    makepkg -si --noconfirm
-    # change out
-    cd ..
-    # git yaourt
-    git clone https://aur.archlinux.org/yaourt.git
-    # change into yaourt
-    cd yaourt
-    # makepkg install
-    makepkg -si --noconfirm
-    # change out
-    cd ..
-}
+	function yaset {
+	    # Install git
+	    sudo pacman -Sy git --noconfirm
+	    # git package-query
+	    git clone https://aur.archlinux.org/package-query.git
+	    # change into package-query
+	    cd package-query
+	    # makepkg install
+	    makepkg -si --noconfirm
+	    # change out
+	    cd ..
+	    # git yaourt
+	    git clone https://aur.archlinux.org/yaourt.git
+	    # change into yaourt
+	    cd yaourt
+	    # makepkg install
+	    makepkg -si --noconfirm
+	    # change out
+	    cd ..
+	}
 
-function yain {
-    yaourt -S $@ --noconfirm --needed
-}
+	function yain {
+	    yaourt -S $@ --noconfirm --needed
+	}
 
-function yainw {
-    yain $(yaourt -Ssq $@)
-}
+	function yainw {
+	    yain $(yaourt -Ssq $@)
+	}
 
-# Search for keyword in the name or description of a package
-function yas {
-    printf "This function is no longer recommended as it does not search the AUR for the regex provided. If you want to search the AUR and enabled package repositories use the pacas function!\n"
-    yaourt -Ss "$@"
-}
+	# Search for keyword in the name or description of a package
+	function yas {
+	    printf "This function is no longer recommended as it does not search the AUR for the regex provided. If you want to search the AUR and enabled package repositories use the pacas function!\n"
+	    yaourt -Ss "$@"
+	}
 
-# Search for a package (wildcard character *, supported)
-function yasw {
-    yaourt -Ssq $@
-}
+	# Search for a package (wildcard character *, supported)
+	function yasw {
+	    yaourt -Ssq $@
+	}
 
-# Remove a package
-function yarm {
-    yaourt -Rsc $@ --noconfirm
-}
+	# Remove a package
+	function yarm {
+	    yaourt -Rsc $@ --noconfirm
+	}
 
-# Upgrade all Yaourt packages and Atom packages
-function yaup {
-    yaourt -Syua --noconfirm --needed
-}
+	# Upgrade all Yaourt packages and Atom packages
+	function yaup {
+	    yaourt -Syua --noconfirm --needed
+	}
 
-# Reinstall all packages using Yaourt
-function yarin {
-    for i in $(pacman -Q | cut -d' ' -f1)
-    do
-         yaourt -Sy $i --noconfirm --overwrite
-    done
-}
+	# Reinstall all packages using Yaourt
+	function yarin {
+	    for i in $(pacman -Q | cut -d' ' -f1)
+	    do
+	         yaourt -Sy $i --noconfirm --overwrite
+	    done
+	}
+fi
--- Shell/pms/20-yay.sh
+++ Shell/pms/20-yay.sh
@@ -3,19 +3,27 @@ function yayup {
 }
 
 alias yayu=yayup
+alias yau=yayup
+alias yaup=yayup
 
 function yayi {
 	yay -S "$@" --noconfirm
 }
 
 alias yayin=yayi
+alias yain=yayi
+alias yai=yayi
 
 function yayr {
 	yay -Rsc "$@" --noconfirm
 }
 
 alias yayrm=yayr
+alias yarm=yayr
+alias yar=yayr
 
 function yays {
 	yay -Ss "$@"
 }
+
+alias yas=yays
