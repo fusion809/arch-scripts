de518d1681767bcb997ad461aec9bc39ec75e0eb
Brenton Horne <brentonhorne77@gmail.com>
2019-03-16 02:37:34 +1000

    Fixing update func

--- Shell/pms/23-update.sh
+++ Shell/pms/23-update.sh
@@ -4,37 +4,11 @@ function update {
 	    yayup
     fi
 
-    # Nix
-    if command -v nix-env &> /dev/null ; then
-    	nixupc
-    fi
-
-    # Guix
-    if command -v guix &> /dev/null ; then
-    	guixupc
-    fi
-
     # APM
     if command -v apm &> /dev/null ; then
     	apmup
     fi
 
-    # Updating GitHub repos
-    if ( command -v git &> /dev/null ) && [[ -d $GHUBO/OpenRA ]]; then
-    	gitoup
-    fi
- 
-    # Update firefox-nightly
-    if [[ -d $HOME/.cache/pacaur/firefox-nightly ]]; then
-         cd $HOME/.cache/pacaur/firefox-nightly
-              fntime=$(ls | grep "bz2$" | cut -d '-' -f 1)
-              actime=$(date +%Y%m%d)
-
-              if ! [[ $fntime == $actime ]]; then
-                   makepkg -sifC --noconfirm
-              fi
-         cd -
-    fi
 
     if command -v flatpak &> /dev/null; then
          flatpak update
@@ -42,4 +16,10 @@ function update {
 
     # GRUB
     update-grub
+
+    pacaup
+
+    if command -v snap &> /dev/null ; then
+	sudo snap refresh
+    fi
 }
