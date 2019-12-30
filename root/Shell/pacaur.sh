a14e87829933389fadb5ac001c60a6d9394be1a5
Brenton Horne <brentonhorne77@gmail.com>
2019-12-30 23:51:34 +1000

    pacaur->yay in root funcs

--- root/Shell/pacaur.sh
+++ root/Shell/pacaur.sh
@@ -1,15 +1,15 @@
 function pacaup {
-	su - fusion809 -c "pacaur -Syu --noconfirm --overwrite --needed --noedit"
+	su - fusion809 -c "yay -Syu --noconfirm --overwrite --needed --noedit"
 }
 
 function update {
-	su - fusion809 -c "pacaur -Syu --noconfirm --overwrite --needed --noedit && apm-bleeding update --no-confirm && vimup"
+	su - fusion809 -c "yay -Syu --noconfirm --overwrite --needed --noedit && apm-bleeding update --no-confirm && vimup"
 }
 
 function pacain {
-	su - fusion809 -c "pacaur -S $@ --noconfirm --overwrite --needed --noedit"
+	su - fusion809 -c "yay -S $@ --noconfirm --overwrite --needed --noedit"
 }
 
 function pacas {
-	su - fusion809 -c "pacaur -Ss $@"
+	su - fusion809 -c "yay -Ss $@"
 }
