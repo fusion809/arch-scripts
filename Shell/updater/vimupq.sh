8eb40bcd19830891d1751df02fee1825f6545f3f
Brenton Horne <brentonhorne77@gmail.com>
2020-03-18 21:48:27 +1000

    Rm vim_baseversion, vim_patchversion from func:

--- Shell/updater/vimupq.sh
+++ Shell/updater/vimupq.sh
@@ -2,8 +2,6 @@ function vim_update_quiet {
     pushd -q /home/fusion809/AUR/gvim-gtk2
     pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
     existing_pkgver=$(grep "^pkgver=" < PKGBUILD | cut -d '=' -f 2)
-    vim_baseversion=$(cat vim.spec | grep "%define.*baseversion" | sed 's/%define.*baseversion\s*//g' | head -n 1)
-    vim_patchversion=$(cat vim.spec | grep "%define.*patchlevel" | sed 's/%define.*patchlevel\s*//g' | head -n 1)
 
     if ! [ $pkgver == $existing_pkgver ]; then
         vimup
