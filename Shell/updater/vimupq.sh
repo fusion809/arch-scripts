f7818ac488936bec6df9598de6978934328f2dbe
Brenton Horne <brentonhorne77@gmail.com>
2020-03-18 15:14:26 +1000

    Fixing pkgver checker for vim

--- Shell/updater/vimupq.sh
+++ Shell/updater/vimupq.sh
@@ -1,12 +1,11 @@
 function vim_update_quiet {
-    pushd -q /home/fusion809/OBS/home:fusion809/vim
+    pushd -q /home/fusion809/AUR/gvim-gtk2
     pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
-    baseversion=$(echo $pkgver | sed 's/\.[0-9]*$//g')
-    patchversion=$(echo $pkgver | sed "s/$baseversion//g" | sed 's/\.//g')
+    existing_pkgver=$(grep "^pkgver=" < PKGBUILD | cut -d '=' -f 2)
     vim_baseversion=$(cat vim.spec | grep "%define.*baseversion" | sed 's/%define.*baseversion\s*//g' | head -n 1)
     vim_patchversion=$(cat vim.spec | grep "%define.*patchlevel" | sed 's/%define.*patchlevel\s*//g' | head -n 1)
 
-    if ! [ $pkgver == ${vim_baseversion}.${vim_patchversion} ]; then
+    if ! [ $pkgver == $existing_pkgver ]; then
         vimup
     fi
     popd -q
