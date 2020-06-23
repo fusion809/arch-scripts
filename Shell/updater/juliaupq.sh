d1b20f32138a5be39e8029200d99e47dad1f3965
Brenton Horne <brentonhorne77@gmail.com>
2020-06-24 02:09:02 +1000

    Fixing juliaupq

--- Shell/updater/juliaupq.sh
+++ Shell/updater/juliaupq.sh
@@ -1,10 +1,13 @@
 function juliaupq {
     pushd -q $HOME/AUR/julia-git
-    pkgver_original=$(cat PKGBUILD | grep ^pkgver= | cut -d '=' -f 2)
-    makepkg -sifC --nocheck --noconfirm
-    pkgver_new=$(cat PKGBUILD | grep ^pkgver= | cut -d '=' -f 2)
-    if [[ $pkgver_original != $pkgver_new ]]; then
-        push "$pkgver_original->$pkgver_new"
+    hash_package=$(cat PKGBUILD | grep ^pkgver= | cut -d '.' -f 6 | sed 's/[a-z0-9]$//g')
+    hash_upstream=$(git -C $GHUBO/julia rev-parse --short HEAD | sed 's/^/g/g')
+    if [[ $hash_package != $hash_upstream ]]; then
+        pkgver_package=$(cat PKGBUILD | grep ^pkgver= | cut -d '=' -f 2)
+        rm *.pkg.tar.xz
+        makepkg -siC --nocheck --noconfirm
+        pkgver_upstream=$(cat PKGBUILD | grep ^pkgver= | cut -d '=' -f 2)
+        push "${pkgver_package}->${pkgver_upstream}"
     fi
     popd -q
 }
