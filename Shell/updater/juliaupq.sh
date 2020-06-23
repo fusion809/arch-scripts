28345997bd7f08cbf7e6c7feaa307110c7a1d116
Brenton Horne <brentonhorne77@gmail.com>
2020-06-24 04:08:20 +1000

    Adding Git repo update line to juliaupq

--- Shell/updater/juliaupq.sh
+++ Shell/updater/juliaupq.sh
@@ -1,6 +1,7 @@
 function juliaupq {
     pushd -q $HOME/AUR/julia-git
     hash_package=$(cat PKGBUILD | grep ^pkgver= | cut -d '.' -f 6 | sed 's/[a-z0-9]$//g')
+    git -C $GHUBO/julia pull origin master -q
     hash_upstream=$(git -C $GHUBO/julia rev-parse --short HEAD | sed 's/^/g/g')
     if [[ $hash_package != $hash_upstream ]]; then
         pkgver_package=$(cat PKGBUILD | grep ^pkgver= | cut -d '=' -f 2)
