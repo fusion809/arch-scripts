4412e3d85fe47b7d0cb808ce41f8767fb7698627
Brenton Horne <brentonhorne77@gmail.com>
2022-12-29 00:15:17 +1000

    Fixing vimup

--- Shell/pms/15-vim.sh
+++ Shell/pms/15-vim.sh
@@ -1,12 +1,13 @@
 function vimup {
 	cda gvim-gtk2
 	pkgver=$(cat PKGBUILD | grep "^pkgver" | sed 's/^pkgver=//g')
-	pkgverupst=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 7 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
+	pkgverupst=$(wget -q https://github.com/vim/vim/tags -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 7 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
 	if [[ $pkgver == $pkgverupst ]]; then
 		return 1
 	fi
 	sed -i -e "s/$pkgver/$pkgverupst/g" PKGBUILD
+	updpkgsums
 	makepkg -sifC --noconfirm
-	rm *.xz *.gz
+	rm *.xz *.gz *.zst *.pkg.tar*
 	push "$pkgver->$pkgverupst"
 }
