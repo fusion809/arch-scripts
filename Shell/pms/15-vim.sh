4b68445924bbcb2d04045c18251eadd91f99ee2d
Brenton Horne <brentonhorne77@gmail.com>
2023-01-01 21:14:21 +1000

    Fixing vimup so that it won't update when the download fails

--- Shell/pms/15-vim.sh
+++ Shell/pms/15-vim.sh
@@ -8,7 +8,7 @@ function vimup {
 	cda gvim-gtk2
 	pkgver=$(cat PKGBUILD | grep "^pkgver" | sed 's/^pkgver=//g')
 	pkgverupst=$(wget -q https://github.com/vim/vim/tags -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 7 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
-	if [[ $pkgver == $pkgverupst ]] || [[ -z $pkgverupst ]]; then
+	if ( [[ $pkgver == $pkgverupst ]] || [[ -z $pkgverupst ]] ); then
 		return 1
 	fi
 	sed -i -e "s/$pkgver/$pkgverupst/g" PKGBUILD
