97ad125c1d7872d73106d87bc9af80b8aae8d735
Brenton Horne <brentonhorne77@gmail.com>
2023-01-01 21:10:51 +1000

    Ensuring that pkgverupstr is not empty

--- Shell/pms/15-vim.sh
+++ Shell/pms/15-vim.sh
@@ -1,6 +1,6 @@
 pkgver=$(cat /data/AUR/gvim-gtk2/PKGBUILD | grep "^pkgver" | sed 's/^pkgver=//g')
 pkgverupst=$(wget -q https://github.com/vim/vim/tags -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 7 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
-if [[ $pkgver != $pkgverupst ]]; then
+if ( [[ $pkgver != $pkgverupst ]] && [[ -n $pkgverupst ]] ); then
 	echo "Vim $pkgverupst is available; run vimup to update gvim-gtk2 to this version"
 fi
 
