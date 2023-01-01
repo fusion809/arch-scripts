e20a8ae5cf52857dfa0fe517dd25528d24de418c
Brenton Horne <brentonhorne77@gmail.com>
2023-01-01 11:02:20 +1000

    Adding message to notify of new version of vim available

--- Shell/pms/15-vim.sh
+++ Shell/pms/15-vim.sh
@@ -1,3 +1,9 @@
+pkgver=$(cat /data/AUR/gvim-gtk2/PKGBUILD | grep "^pkgver" | sed 's/^pkgver=//g')
+pkgverupst=$(wget -q https://github.com/vim/vim/tags -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 7 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
+if [[ $pkgver != $pkgverupst ]]; then
+	echo "Vim $pkgverupst is available; run vimup to update gvim-gtk2 to this version"
+fi
+
 function vimup {
 	cda gvim-gtk2
 	pkgver=$(cat PKGBUILD | grep "^pkgver" | sed 's/^pkgver=//g')
