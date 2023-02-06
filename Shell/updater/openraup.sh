58fe3f186cc342c48a63c9c9315a782f6024a723
Brenton Horne <brentonhorne77@gmail.com>
2023-02-06 13:40:46 +1000

    Fixing to openraup func

--- Shell/updater/openraup.sh
+++ Shell/updater/openraup.sh
@@ -4,7 +4,7 @@ function openraup {
     upno=$(git rev-list --count HEAD)
     uphash=$(git log | head -n 1 | cut -d ' ' -f 2 | head -c 7)
     upver="${upno}.git.${uphash}"
-    pkgver=$(cat PKGBUILD | grep "^pkgver=" | cut -d '=' -f 2)
+    pkgver=$(cat $HOME/AUR/openra-wts-git/PKGBUILD | grep "^pkgver=" | cut -d '=' -f 2)
 
     if [[ $upver != $pkgver ]]; then
         cda openra-wts-git
