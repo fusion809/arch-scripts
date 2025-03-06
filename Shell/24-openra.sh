622cff6a7836f14b51f7e05e91b2580480a4645b
Brenton Horne <brentonhorne77@gmail.com>
2025-03-06 15:22:57 +1000

    openra-wts-git is fixed now

--- Shell/24-openra.sh
+++ Shell/24-openra.sh
@@ -5,7 +5,8 @@ hashup=$(git log | head -n 1 | cut -d ' ' -f 2 | head -c 7)
 popd -q
 pkgverup="${comnoup}.git.${hashup}"
 
-pkgver=$(ls ~/Applications | grep "Red-Alert" | cut -d '-' -f 4)
+#pkgver=$(ls ~/Applications | grep "Red-Alert" | cut -d '-' -f 4)
+pkgver=$(cat $HOME/AUR/openra-wts-git/PKGBUILD | grep "^pkgver=" | cut -d '=' -f 2)
 
 if [[ $pkgverup != $pkgver ]]; then
 	echo "OpenRA $pkgverup is available; run openraup to update AppImages"
--- Shell/updater/openraup.sh
+++ Shell/updater/openraup.sh
@@ -9,21 +9,21 @@ function openraup {
     upno=$(git rev-list --count HEAD)
     uphash=$(git log | head -n 1 | cut -d ' ' -f 2 | head -c 7)
     upver="${upno}.git.${uphash}"
-    #pkgver=$(cat $HOME/AUR/openra-wts-git/PKGBUILD | grep "^pkgver=" | cut -d '=' -f 2)
-    pkgver=$(cat /mnt/d/GitHub/others/OpenRA/VERSION)
+    pkgver=$(cat $HOME/AUR/openra-wts-git/PKGBUILD | grep "^pkgver=" | cut -d '=' -f 2)
+    #pkgver=$(cat /mnt/d/GitHub/others/OpenRA/VERSION)
 
     if [[ $upver != $pkgver ]]; then
-        #cda openra-wts-git
-        #makepkg -sifC --noconfirm
-        #rm *.zst
-	rm ~/Applications/OpenRA*.AppImage
-	cdgo OpenRA
-	make clean
-	make
-	cd packaging/linux
-	./buildpackage.sh $upver ~/Applications
-	appimrname "Red Alert" $upver
-	appimrname "Tiberian Dawn" $upver
-	appimrname "Dune 2000" $upver
+        cda openra-wts-git
+        makepkg -sifC --noconfirm
+        rm *.zst
+	#rm ~/Applications/OpenRA*.AppImage
+	#cdgo OpenRA
+	#make clean
+	#make
+	#cd packaging/linux
+	#./buildpackage.sh $upver ~/Applications
+	#appimrname "Red Alert" $upver
+	#appimrname "Tiberian Dawn" $upver
+	#appimrname "Dune 2000" $upver
     fi
 }
